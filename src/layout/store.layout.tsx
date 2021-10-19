a7d35d6974a8ebe6aad136cea6dd2987f575c2cc
Gabriel Antunes <gabriel.antunes@compasso.com.br>
2021-10-19 15:19:31 -0300

    add store base layout

--- src/layout/store.layout.tsx
+++ src/layout/store.layout.tsx
@@ -1,5 +1,33 @@
-function StoreLayout() {
-  return <></>;
+import { Box, Card, Container, Grid } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+import StoreAppBar from "./components/store_app_bar.component";
+
+type StoreLayoutProps = {
+  children: React.ReactNode;
+};
+
+function StoreLayout({ children }: StoreLayoutProps) {
+  const classes = useClasses();
+
+  return (
+    <Box className={classes.container}>
+      <StoreAppBar />
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Card component="main">{children}</Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
+
+const useClasses = makeStyles({
+  container: {
+    minHeight: "100vh",
+  },
+});
 
 export default StoreLayout;
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import StoreLayout from "layout/store.layout";
 import { AppRouteItem } from "types/route";
-// import StoreLayout from "layout/store.layout";
 
 function AppRoutes({ routes }: { routes: AppRouteItem[] }): React.ReactElement {
   return (
     <Router>
-      <Switch>
-        {routes.map(({ path, component: Component, key }) => (
-          <Route path={path} exact component={Component} key={key} />
-        ))}
-      </Switch>
+      <StoreLayout>
+        <Switch>
+          {routes.map(({ path, component: Component, key }) => (
+            <Route path={path} exact component={Component} key={key} />
+          ))}
+        </Switch>
+      </StoreLayout>
     </Router>
   );
 }
